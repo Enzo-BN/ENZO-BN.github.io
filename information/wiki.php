0559dd63856d42addfcdae7287a56294771a5a84
Nevxu <78749383+nevxu@users.noreply.github.com>
2022-09-04 18:10:23 +0200

    Add files via upload

---
+++ information/wiki.php
@@ -0,0 +1,63 @@
+<div class="col-lg-3 col-md-3 d-flex flex-column align-items-center justify-content-center block mt-4">
+                    <a href="https://discord.com/invite/histeria">DISCORD</a>
+                    <p>784  joueurs en ligne </p>
+                </div>
+
+
+
+                <h3 class="mt-0 text-body">Merci de bien lire le règlement pour ne pas être sanctionné !</h3>
+                                        <h5 class="text-secondary"><small><i>Le 24 décembre 2021 à 12:56</small></i></h5>
+                                        <h5><i>Règlement Discord</i></h5></br>
+  <h5>I - Profil utilisateur :</h5></br>
+
+    1.1 - Vous devez avoir une photo de profil correct (pas de nudité, pas de pornographie ou autre).<br /></br>
+    1.2 - Votre pseudonyme doit être lisible, sans caractère spéciaux, facilement mentionnable. (Demander à un membre de notre équipe pour le changer !).<br /></br>
+    1.3 - L'usurpation d'identité est interdite, cela conduira à un bannissement définitif sans préavis.<br /></br>
+    1.4 - L'utilisation d'un double compte pour accéder à nos service est strictement interdit, cela à nu bannissement définitif sans préavis et non-révocable.<br /></br>
+    1.5 - Toute forme de publicité est interdite (tolérance pour la publicité dans les statuts à condition que cela soit légal).<br /></br>
+    1.6 - La diffusion d'informations à caractère privée sans l'accord de cette dernière est interdite.<br /></br>
+    1.7 - Les menaces et les tentatives de nuire à une société ou à une personne sont interdite et résultera à un bannissement définit sans préavis et non-révocable<br /></br>
+
+    <h5><i>Règlement Minecraft :</i></h5></br>
+    <h5>I - Mute :</h5></br>
+
+    1.1 - Insulte quelconque : 5-15m selon l'insulte.<br /></br>
+    1.2 - Le Spam : 5-15m selon le spam.<br /></br>
+    1.3 - Le flood : 5m.<br /></br>
+    1.4 - @ here : 30m.<br /></br>
+    1.5 - Les arnaques sont interdites.<br /></br>
+    1.6 - Demander d'être give : 10min.<br /></br>
+    1.7 - Demander Reset-Mine : 10m.<br /></br>
+
+    <h5>II - Bans :</h5></br>
+
+    2.1 - Usebug portillons, hache,pioche, etc (SAUF ENDER PEARL) : 1 jour.<br /></br>
+    2.2 - Cheat : 1 mois.<br /></br>
+    2.3 - Déco-Combat/Crash-Co : 14 jours + Clear Stuff.<br /></br>
+    2.4 - Trap no heal : 7 jours + Cut Trap<br /></br>
+    2.5 - Descendre en dessous des 8ms une sourie : 7 jours.<br /></br>
+    2.6 - DoubleCompte : Ban Permanence.<br /></br>
+    2.7 - Menace DDOS etc : 30 jours.<br /></br>
+    2.8 - Trahison : 7 jours + Remboursement.<br /></br>
+    2.9 - FreeKill / FreePowers : 2 jours + Reset Power.<br /></br>
+    2.10 - Pub : 1 jour.<br /></br>
+    2.11 - BackSafeZone : 1 jour.<br /></br>
+    2.12 - No-Stuff : 1 jour.<br /></br>
+    2.13 - F sethome hors de sa Trap : 1 jour + Unclaim.<br /></br>
+    2.14 - No-Clip : 2 mois.<br /></br>
+    2.15 - Arnaque échange : 3 jours + Clear Stuff + Remboursement.<br /></br>
+
+    <h5>III - Liste autorisée</h5></br>
+
+    3.1 - Use bug F5.<br /></br>
+    3.2 - Use bug Ender Pearl<br /></br>
+    3.3 - Arnaque hors salle d'échange.<br /></br>
+
+    <h4>Rejoindre ce serveur signifie en respecter les règles. (Le staff ne prend en aucun cas les demandes de supports en MP.)</h4><br/>
+En cas de manquement à l'une des règle veuillez en faire part immédiatement à un modérateur, administrateurs ou tout autres membres du staff de <a href="https://discord.gg/HeWwMYA3eM"><b>Asmonia</b></a> pour faire le nécessaire.                            </div>
+                                </div>
+                                                        
+                        </div>
+                    </div>                  
+                </div>
+            </div>
